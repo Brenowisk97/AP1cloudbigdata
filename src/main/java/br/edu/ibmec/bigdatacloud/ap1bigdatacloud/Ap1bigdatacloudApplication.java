e65557e486bf17143c669d6362ca390e218b6112
Breno de Souza <brenorego10@gmail.com>
2024-09-27 12:26:17 -0300

    primeiro commit

---
+++ src/main/java/br/edu/ibmec/bigdatacloud/ap1bigdatacloud/Ap1bigdatacloudApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ibmec.bigdatacloud.ap1bigdatacloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ap1bigdatacloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ap1bigdatacloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ibmec/bigdatacloud/ap1bigdatacloud/Ap1bigdatacloudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ibmec.bigdatacloud.ap1bigdatacloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap1bigdatacloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
